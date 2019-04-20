4711601c36ca4c9827bafe7e78ecc81d5af74f13
Damon Wu <damonwu0605@gmail.com>
2019-04-20 22:40:57 +1000

    [SAGA-07][Damon Wu] added local storage (convenient to check the data)

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,14 @@ import {
   compose
 } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { fromJS } from 'immutable';
+import { loadState, saveState } from './localStorage';
 import { todo, status } from '../reducers';
 import { rootSaga } from '../sagas';
 
+const presistState = fromJS(loadState()); // initialised fromat need to be immutable object !!!
+// console.log('load state data ', presistState);
+// loadState(); // default load data from local storage !!
 const reducers = combineReducers({
   todo,
   status,
@@ -19,8 +24,13 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducers,
+  presistState, // load state data from redux store
   compose(applyMiddleware(sagaMiddleware), reduxDevTool)
 );
+
+store.subscribe(() => {
+  saveState(store.getState()); // save state into reux store
+});
 
 sagaMiddleware.run(rootSaga);
 export default store;
